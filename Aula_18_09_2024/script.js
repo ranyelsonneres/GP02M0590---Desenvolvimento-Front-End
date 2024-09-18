b6c8856d40fbf103dffb42e48bea24a3394b3110
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2024-09-18 11:39:01 -0300

    Add files via upload

---
+++ Aula_18_09_2024/script.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+    var boasVinda = "Seja bem-vindo(a)!";
+    console.log(boasVinda);
+
+    alert(boasVinda);
+
+    var letra = document.getElementById('letra-texto'); 
+    letra.innerHTML = "RHCP - Californication <br>" 
+    + "Psychic spies from China try to steal your mind's elation <br>"
+    + "And little girls from Sweden dream of silver screen quotation";   
+};
